390b4a5509b68251ba72b57ef2fb7040767c5cd3
Ashley King <ashleyot@gmail.com>
2019-01-29 15:30:19 -0500

    working on assignment 4

--- assignment_4_Ashley_King_winter_2019.py
+++ assignment_4_Ashley_King_winter_2019.py
@@ -1,9 +1,42 @@
+'''
+Algorithm:
+
+* create a range of years from 1897 to 2012
+* for each year in the range
+	-- instantiate list of 0's to hold count of each digit in year (digit_count)
+	-- instatiate a string that casts the year int into a string
+	-- create a list to hold the duplicated digits (new_list)
+	* for each digit in the year string
+	  -- set number = the digit converted to an int
+	  -- increment the corresponding element in digit_count by 1
+ 	  * loop over range of length of digit_count
+	    -- if digit_count at that index > 1. append that element to new_list
+	       (this is the list of duplicated numbers)
+          * if the length of new_list == 0, no number were duplicated, print year andduplicate status to screen
+	    -- else (new_list length > 1)
+	       -- create an empty string
+	       * for each item in new_list, append spaces and item to new_string
+               print the year + the string that shows which numbers are duplicated
+'''
+
 year_range = range(1987, 2013)
 
 for year in year_range: #beginning year loop
     digit_count = [0] * 10
     year_string = str(year)
+    new_list = []
     for digit in year_string:
         number = int(digit)
         digit_count[number] += 1
-    for digi in digit_count:
+    for i in range(len(digit_count)):
+
+        if digit_count[i] > 1:
+            new_list.append(i)
+    if len(new_list) == 0:
+        print(str(year) + " - no duplicates")
+    else:
+        new_string = ""
+        for item in new_list:
+            new_string += " " + str(item) + " "
+        print(str(year) + " - duplicated number(s):" + new_string)
+
---
+++ practice.py
@@ -0,0 +1,21 @@
+x = []
+y = []
+
+for i in range(5,  60,  10) :
+     fi = i / 10.0
+     x.append(fi)
+     y.append(fi)
+
+d = (i/10.0 for i in range(5,60, 10))
+e = (i/10.0 for i in range(5,60, 10))
+
+print('\t   '),
+for i in range(5, 60, 10):
+    fi = i/10.0
+    print(fi),
+for i in range(5, 60, 10):
+    fi = i/10.0
+    print '\n','{:02}'.format(fi),' ',
+    for j in range(5, 60, 10):
+        fj = j/10.0
+        print '{:02}'.format(fi*fj),
