ae083bf52930247975fca5f53fa82493d032f3e3
Ashley King <ashleyot@gmail.com>
2019-01-30 09:48:47 -0500

    assignment 4 - found a possible solution

--- practice.py
+++ practice.py
@@ -1,42 +1,58 @@
-x = []
-y = []
-
-for i in range(5,  60,  10) :
-     fi = i / 10.0
-     x.append(fi)
-     y.append(fi)
-
-# for index in range(len(x)):
-#     print x[index]
-for index in range(len(x)):
-    row_out = "{0:10.2f}".format(x[index])
-    print(row_out),
-print'\n'
-for index in range(len(y)):
-    print('{:0.2f}'.format(y[index]))
-    for jindex in range(len(x)):
-        print('{:0.2f}'.format(y[index] + x[jindex])),
-
-# d = (i/10.0 for i in range(5,60, 10))
-# e = (i/10.0 for i in range(5,60, 10))
+# x = []
+# y = []
 #
-# print('\t   '),
-# for i in range(5, 60, 10):
-#     fi = i/10.0
-#     print("\t\t" + str(fi)),
-# for i in range(5, 60, 10):
-#     fi = i/10.0
-#     print '\n','{:02}'.format(fi),' ',
-#     for j in range(5, 60, 10):
-#         fj = j/10.0
-#         print '\t\t{:02}'.format(fi*fj),
+# for i in range(5,  60,  10) :
+#      fi = i / 10.0
+#      x.append(fi)
+#      y.append(fi)
+#
+# # for index in range(len(x)):
+# #     print x[index]
+# for index in range(len(x)):
+#     row_out = "{0:10.2f}".format(x[index])
+#     print(row_out),
+# print'\n'
+# for index in range(len(y)):
+#     print('{:0.2f}'.format(y[index]))
+#     for jindex in range(len(x)):
+#         print('{:0.2f}'.format(y[index] + x[jindex]))
+
+# horiz_row = (i/10.0 for i in range(5,60, 10))
+# vert_row = (i/10.0 for i in range(5,60, 10))
+# #
+# # print('\t   '),
+# # for el in horiz_row:
+# #     print("\t" + str(el), end=" ")
+# #
+# # for item in vert_row:
+# #     print('\n{:02}'.format(item), end=" ")
+# for j in horiz_row:
+#     print('this')
+
+#     print ('\t{:02}'.format(i*j)),
+
+header = []
+for i in range(1, 10):
+    header.append('{0:>2}'.format(i))
+
+rows = []
+for i in range(1, 10):
+   row = []
+   for q in range(1, 10):
+     result = '{0:>2}\t'.format(i*q)
+     row.append(result)
+   rows.append(row)
+
+print('\t{0:>2}'.format('\t'.join(header)))
+for idx, row in enumerate(rows):
+   print('{0:>2}\t{1}'.format(idx+1, ''.join(row)))
+
+
+
+
+
+
+
+
 
         # row_out = "item #{} is {:15.2f}".format(index, various_nums[index])
-print '\n'
-for i in range(1, 10):
-    print "i =", i, ":",  # Note the comma at the end
-
-    for j in range(1, 10):
-        print "{:2d}".format(i * j),  # Note the comma at the end
-
-    print
