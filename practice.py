e5bca902ec0260ccb6200133f614f494f2a030ce
Ashley King <ashleyot@gmail.com>
2019-01-30 12:36:46 -0500

    assignment 4 - found a possible solution

--- practice.py
+++ practice.py
@@ -31,28 +31,47 @@
 
 #     print ('\t{:02}'.format(i*j)),
 
-header = []
-for i in range(1, 10):
-    header.append('{0:>2}'.format(i))
+# header = []
+# for i in range(1, 10):
+#     header.append('{0:>2}'.format(i))
+#
+# rows = []
+# for i in range(1, 10):
+#    row = []
+#    for q in range(1, 10):
+#      result = '{0:>2}\t'.format(i*q)
+#      row.append(result)
+#    rows.append(row)
+#
+# print('\t{0:>2}'.format('\t'.join(header)))
+# for idx, row in enumerate(rows):
+#    print('{0:>2}\t{1}'.format(idx+1, ''.join(row)))
 
-rows = []
-for i in range(1, 10):
-   row = []
-   for q in range(1, 10):
-     result = '{0:>2}\t'.format(i*q)
-     row.append(result)
-   rows.append(row)
+# row_out = "item #{} is {:15.2f}".format(index, various_nums[index])
 
-print('\t{0:>2}'.format('\t'.join(header)))
-for idx, row in enumerate(rows):
-   print('{0:>2}\t{1}'.format(idx+1, ''.join(row)))
+# print statement to print headings
+# outer loop process one complete horiz row in each pass
+# separate print statement before inner loop to print row label on left
+# inner loop process individual components of single row & format/output
 
 
+x = []
+y = []
+
+for i in range(5,  60,  10) :
+     fi = i / 10.0
+     x.append(fi)
+     y.append(fi)
+print('\t   ', end=' ')
+for h in x:
+    print("\t" + str(h), end = " ")
+print()
+for h in x:
+    print(h, end=' ')
+    for v in y:
+        print('\t {:0.2f}'.format(v*h), end=" ")
+    print()
 
 
 
 
-
-
-
-        # row_out = "item #{} is {:15.2f}".format(index, various_nums[index])
